4cc6f180f47d12d2188b9bb64162dc742c6e82c6
Codetatorang <102163869+Codetatorang@users.noreply.github.com>
2023-04-21 15:01:29 +0800

    completed day32 lecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DeliveryOrder } from './components/models';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'day32-lecture';
+  orders: DeliveryOrder[] = []
+
+  processNewOrder(order:DeliveryOrder){
+    this.orders.unshift(order)
+  }
 }
--- src/app/components/delivery.component.ts
+++ src/app/components/delivery.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, Output } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DeliveryOrder } from './models';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-delivery',
@@ -7,9 +9,13 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./delivery.component.css']
 })
 export class DeliveryComponent implements OnInit {
+  form!: FormGroup
+
+  @Output()
+  onNewDeliveryOrder = new Subject<DeliveryOrder>()
 
   fb: FormBuilder
-  form!: FormGroup
+  itemArray!: FormArray
   //private/public constructor params will auto inject, therefore not needed to declare variable
   constructor(fb: FormBuilder) { //more injections are added with ,
     this.fb = fb
@@ -19,21 +25,55 @@ export class DeliveryComponent implements OnInit {
     //this method is called when the component is created
     //for initialization
     this.form = this.createForm()
+
   }
 
   private createForm(): FormGroup {
+    this.itemArray = this.fb.array([])
     return this.fb.group({
-      name: this.fb.control<string>(''),
-      address: this.fb.control<string>(''),
-      email: this.fb.control<string>(''),
-      deliveryDate: this.fb.control<string>(''),
-      comments: this.fb.control<string>('')
-
+      name: this.fb.control<string>('', [Validators.required, Validators.minLength(3)]),
+      address: this.fb.control<string>('1 bedrock', [Validators.required]),
+      email: this.fb.control<string>('fred@gmail.com', [Validators.required, Validators.email]),
+      session: this.fb.control<string>('PM', [Validators.required]),
+      deliveryDate: this.fb.control<string>('', [Validators.required]),
+      priority: this.fb.control<Boolean>(false),
+      insurance: this.fb.control<Boolean>(false),
+      comments: this.fb.control<string>('no comments'),
+      orderItems: this.itemArray
     })
   }
 
-  processDelivery(){
-    const delivery = this.form.value
+  private createOrderItem(): FormGroup {
+    return this.fb.group({
+      item: this.fb.control<string>('', [Validators.required]),
+      quantity: this.fb.control<number>(1, [Validators.required, Validators.min(1)])
+    })
+  }
+
+  processDelivery() {
+    const delivery = this.form.value as DeliveryOrder
+    this.onNewDeliveryOrder.next(delivery)
     console.info(">>> delivery: ", delivery)
+    this.ngOnInit()
+    this.form.reset()
+  }
+
+  addItem() {
+    const orderItem = this.createOrderItem()
+    this.itemArray.push(orderItem)
+  }
+  hasError(cn: string): boolean {
+    return !!(this.form.get(cn)?.invalid && this.form.get(cn)?.dirty)
+  }
+  deleteItem(idx: number) {
+    this.itemArray.removeAt(idx)
+  }
+  isFormInvalid(): boolean {
+    const dd = this.form.get('deliveryDate')?.value
+    if (!dd)
+      return true
+    const deliveryDate = new Date(dd)
+    const today = new Date()
+    return this.form.invalid && (deliveryDate < today)
   }
 }
